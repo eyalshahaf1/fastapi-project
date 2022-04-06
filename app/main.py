8a8f6473c932750ff7b7d1fd6aff1072f4e9d41f
eyalshahaf1 <eyal.shahaf1@gmail.com>
2022-04-06 20:43:38 +0300

    update Procfile erroe

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from . import models
 # from .database import engine
 from .routers import post, user, auth, vote
-# from .config import settings
+from .config import settings
 
 #Not required when using alembic
 # models.Base.metadata.create_all(bind=engine)
